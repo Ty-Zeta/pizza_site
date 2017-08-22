9475c52636ec6b003abc50f2c6b6ae66c2c06097
Ty-Zeta <tyofzeta@gmail.com>
2017-08-22 15:44:40 -0400

    reached 2nd page, but can only pick one of each topping

--- app.rb
+++ app.rb
@@ -5,7 +5,13 @@ get '/' do
     erb :index
 end
 
-post '/chooding' do
-    session[:options] = params[:sauce]
+post '/choosing' do
+    session[:sauce_type] = params[:sauce]
+    session[:meat_type] = params[:meat]
+    session[:veg_type] = params[:veg]
+    redirect '/confirm'
 end
 
+get '/confirm' do
+    erb :confirm, locals: {sauce: session[:sauce_type], meat: session[:meat_type], veg: session[:veg_type]}
+end
