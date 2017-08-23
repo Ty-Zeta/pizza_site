2bb28ba7f71a8b9c62fb0f78e134c963f0af42be
Ty-Zeta <tyofzeta@gmail.com>
2017-08-22 21:48:42 -0400

    added pics to index, values not being passed to results

--- app.rb
+++ app.rb
@@ -15,3 +15,15 @@ end
 get '/confirm' do
     erb :confirm, locals: {sauce: session[:sauce_type], meat: session[:meat_type], veg: session[:veg_type]}
 end
+
+post '/confirm' do
+    session[:sauce_choice] = params[:sauce_yes] 
+    session[:meat_choice] = params[:meat_yes]
+    session[:veg_choice] = params[:veg_yes]
+    p "#{veg_choice} HEY DUMMY!!!"
+    redirect '/results'
+end
+
+get '/results' do
+    erb :results, locals: {sauce_yes: session[:sauce_choice], meat_yes: session[:meat_choice], veg_yes: session[:veg_choice]}
+end
