69ef8bb33b11606015099df04bb151a7ccbbb591
Ty-Zeta <tyofzeta@gmail.com>
2017-08-25 12:46:22 -0400

    added size radio buttons

--- app.rb
+++ app.rb
@@ -29,6 +29,7 @@ get '/index' do
 end
 
 post '/choosing' do
+    session[:pizza_size] = params[:size]
     session[:sauce_type] = params[:sauce]
     session[:meat_type] = params[:meat]
     session[:veg_type] = params[:veg]
@@ -36,10 +37,11 @@ post '/choosing' do
 end
 
 get '/confirm' do
-    erb :confirm, locals: {sauce: session[:sauce_type], meat: session[:meat_type], veg: session[:veg_type]}
+    erb :confirm, locals: {sauce: session[:sauce_type], meat: session[:meat_type], veg: session[:veg_type], size: session[:pizza_size]}
 end
 
 post '/confirm' do
+    session[:pizza_choice] = params[:size_radio]
     session[:sauce_choice] = params[:sauce_radio] 
     session[:meat_choice] = params[:meat_radio]
     session[:veg_choice] = params[:veg_radio]
@@ -47,5 +49,5 @@ post '/confirm' do
 end
 
 get '/results' do
-    erb :results, locals: {sauce_final: session[:sauce_choice], meat_final: session[:meat_choice], veg_final: session[:veg_choice], take_pizza_here: session[:del_address]}
+    erb :results, locals: {sauce_final: session[:sauce_choice], meat_final: session[:meat_choice], veg_final: session[:veg_choice], take_pizza_here: session[:del_address], size_final: session[:pizza_choice]}
 end
